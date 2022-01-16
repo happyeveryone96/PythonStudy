d5f689308757634cbed6f315971acced02ad194a
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-16 22:18:32 +0900

    변수

---
+++ theory/variable.py
@@ -0,0 +1,14 @@
+a = 2
+a_string = 'like this'
+a_number = 3
+a_float = 3.12
+a_boolean = False
+a_none = None
+# a = 'like this'
+b = 3
+c = True
+d = False
+print(type(a_number))
+print(type(a_float))
+print(type(a_boolean))
+print(a+b)
