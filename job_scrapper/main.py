a6f4599051179eff050a0f93d66a649f5e8b5385
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-18 09:56:54 +0900

    feat: BeautifulSoup을 사용해 Indeed 페이지 추출

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -1,5 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
 
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("ul",{"class":"pagination-list"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+  spans.append(page.find("span"))
+spans = spans[:-1]
+
+print(spans)
