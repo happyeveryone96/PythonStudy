54a91e9f6d7eeb9500b511965255d3047bf41bb6
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-18 10:42:56 +0900

    feat: indeed 모듈 추가

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -1,16 +1,5 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
+last_indeed_pages = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("ul",{"class":"pagination-list"})
-
-pages = pagination.find_all('a')
-spans = []
-for page in pages:
-  spans.append(page.find("span"))
-spans = spans[:-1]
-
-print(spans)
+extract_indeed_jobs(last_indeed_pages)
