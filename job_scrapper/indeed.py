a59eabecede11058339dc41fc4856ce3b104654c
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-19 20:52:30 +0900

    feat: SO 모듈 추가

--- job_scrapper/indeed.py
+++ job_scrapper/indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
 
-def extract_indeed_pages():
+def get_last_page():
   result = requests.get(URL)
 
   soup = BeautifulSoup(result.text, "html.parser")
@@ -29,14 +29,20 @@ def extract_job(html):
     job_id = html["data-jk"]
     return {'title':title,'company':company, 'location':location, 'link': f"https://www.indeed.com/viewjob?jk={job_id}"}
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
   jobs = []
   for page in range(last_page):
-    print(f"Scrapping page {page}")
+    print(f"Scrapping Indeed: Page: {page}")
     result = requests.get(f"{URL}&start={page*LIMIT}")
     soup = BeautifulSoup(result.text, "html.parser")
     results = soup.find_all("a", {"class": "fs-unmask"})
     for result in results:
       job = extract_job(result)
       jobs.append(job)
+  return jobs
+
+def get_jobs():
+  last_page = get_last_page()
+  jobs = extract_jobs(last_page)
+  jobs = [x for x in jobs if x is not None]
   return jobs
